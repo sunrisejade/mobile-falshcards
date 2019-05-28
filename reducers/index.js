908641a931d26ea6622519b6549419cb0381476d
Jane Shan <moonfallmaple@hotmail.com>
2019-05-29 06:17:46 +0800

    Add files via upload

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,10 @@
 // REDUCERS
 
-import { RECIEVE_DECKS, ADD_DECK, ADD_CARD } from '../actions'
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions'
 
 function decks (state = {}, action) {
     switch (action.type) {
-        case RECIEVE_DECKS:
+        case RECEIVE_DECKS:
             return {
                 ...action.decks
             }
